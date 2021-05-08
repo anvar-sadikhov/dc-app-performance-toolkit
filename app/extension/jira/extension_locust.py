acf09e096090ea31aa290987b3b833e31650b858
anvar.sadikhov <anvar.sadikhov@softgile.com>
2021-05-08 05:44:57 +0400

    no jql+locust info

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -8,17 +8,17 @@ logger = init_logger(app_type='jira')
 @run_as_specific_user(username='admin', password='admin')  # run as specific user
 def app_specific_action(locust):
     r = locust.get('/rest/asanrest/1.0/message', catch_response=True)  # call app-specific GET endpoint
-    # content = r.content.decode('utf-8')   # decode response content
+    content = r.content.decode('utf-8')   # decode response content
 
-    # token_pattern_example = '"token":"(.+?)"'
-    # id_pattern_example = '"id":"(.+?)"'
-    # token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
-    # id = re.findall(id_pattern_example, content)    # get ID from response using regexp
+    token_pattern_example = '"token":"(.+?)"'
+    id_pattern_example = '"id":"(.+?)"'
+    token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
+    id = re.findall(id_pattern_example, content)    # get ID from response using regexp
 
-    # logger.locust_info(f'token: {token}, id: {id}')  # log info for debug when verbose is true in jira.yml file
-    # if 'assertion string' not in content:
-    #     logger.error(f"'assertion string' was not found in {content}")
-    # assert 'assertion string' in content  # assert specific string in response content
+    logger.locust_info(f'token: {token}, id: {id}')  # log info for debug when verbose is true in jira.yml file
+    if 'assertion string' not in content:
+        logger.error(f"'assertion string' was not found in {content}")
+    assert 'assertion string' in content  # assert specific string in response content
 
     # body = {"id": id, "token": token}  # include parsed variables to POST request body
     # headers = {'content-type': 'application/json'}
